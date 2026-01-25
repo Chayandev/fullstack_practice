cf9a5a5a29aa6b5dd08e58b026f98d2c6c8f497b
Chayandev Bera <chayandevbera@gmail.com>
2026-01-25 22:29:10 +0530

    Added the typedef

--- GraphQL/server/index.js
+++ GraphQL/server/index.js
@@ -1,12 +1,10 @@
 import { ApolloServer } from "apollo-server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-
+import { typeDefs } from "./schema.js";
 // server setup
 const server = new ApolloServer({
-    //typeDefs- defination of tyeps of data ex:user/author, we expose to graph, which clinet query on.
-     
-
-    //resolvers
+  typeDefs, //-defination of tyeps of data ex:user/author, we expose to graph, which clinet query on.
+  //resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
--- GraphQL/server/schema.js
+++ GraphQL/server/schema.js
@@ -1,1 +1,22 @@
- 
+export const typeDefs = `#graphql
+    type Game {
+     id: ID! # the exclamation mark means this field is non-nullable
+     title: String!
+     platform: [String]!
+    }
+    type Review {
+     id:ID!
+     raing:Int!,
+     content:String!
+   }
+    type Author{
+     id:ID!
+     name:String!
+     verified:Boolean
+   }
+    type Query { #this is the defination of the graph entry point which need to be mapped with the resolver fucntions
+     games: [Game] 
+     reviews:[Review]
+     authors:[Author]
+    }
+`;
